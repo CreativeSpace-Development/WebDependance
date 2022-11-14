483c3b1372969c19dc5cc89fb1a4aadfdb6ec338
Kool Kreator <kieran.wells@stu.meade.kyschools.us>
2022-11-14 13:47:35 -0500

    Update insert.js

--- insert.js
+++ insert.js
@@ -7,6 +7,13 @@ if(!document.getElementById('bootstrap-scripts')) {
     script.crossOrigin="anonymous";
     document.head.appendChild(script);
 }
+if(!document.getElementById('jquery-scripts')) {
+    var script = document.createElement('script');
+    script.id = 'jquery-scripts';
+    script.type = 'text/javascript';
+    script.src = "https://code.jquery.com/jquery-3.6.1.min.js";
+    document.head.appendChild(script);
+}
 if(!document.getElementById('bootstrap-styles')) {
     var link = document.createElement('link');
     link.id = 'bootstrap-styles';
@@ -14,5 +21,12 @@ if(!document.getElementById('bootstrap-styles')) {
     link.href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css';
     script.integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi";
     script.crossOrigin="anonymous";
+    document.head.appendChild(link);
+}
+if(!document.getElementById('W3-styles')) {
+    var link = document.createElement('link');
+    link.id = 'W3-styles';
+    link.rel = 'stylesheet';
+    link.href = 'https://www.w3schools.com/w3css/4/w3.css';
     document.head.appendChild(link);
 }
