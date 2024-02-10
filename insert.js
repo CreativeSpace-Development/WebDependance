cbf32ec240df2f0d96d5f05dacf3dd5396469599
Kool Kreator <creativespacedev@outlook.com>
2024-02-10 14:50:37 -0500

    Bumped to latest version

--- insert.js
+++ insert.js
@@ -2,8 +2,8 @@ if(!document.getElementById('bootstrap-scripts')) {
     var script = document.createElement('script');
     script.id = 'bootstrap-scripts';
     script.type = 'text/javascript';
-    script.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"; 
-    script.integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3";
+    script.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"; 
+    script.integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL";
     script.crossOrigin="anonymous";
     document.head.appendChild(script);
 }
@@ -11,15 +11,15 @@ if(!document.getElementById('jquery-scripts')) {
     var script = document.createElement('script');
     script.id = 'jquery-scripts';
     script.type = 'text/javascript';
-    script.src = "https://code.jquery.com/jquery-3.6.1.min.js";
+    script.src = "https://code.jquery.com/jquery-3.7.1.min.js";
     document.head.appendChild(script);
 }
 if(!document.getElementById('bootstrap-styles')) {
     var link = document.createElement('link');
     link.id = 'bootstrap-styles';
     link.rel = 'stylesheet';
-    link.href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css';
-    script.integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi";
+    link.href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css';
+    script.integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN";
     script.crossOrigin="anonymous";
     document.head.appendChild(link);
 }
